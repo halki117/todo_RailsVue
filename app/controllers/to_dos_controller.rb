893105d133e0d08c3d174d632a456f941f42f144
halki <s.halki0203@icloud.com>
2020-05-30 14:50:47 +0900

    update

---
+++ app/controllers/to_dos_controller.rb
@@ -0,0 +1,45 @@
+class ToDosController < ApplicationController
+  
+  def index
+    @to_dos = ToDo.all
+  end
+
+  def new
+    @to_do = ToDo.new
+  end
+
+  def create
+    @to_do = ToDo.new(to_do_params)
+    if @to_do.save
+      redirect_to to_dos_url
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @to_do = ToDo.find_by(id: params[:id])
+  end
+
+  def update
+    @to_do = ToDo.find_by(id: params[:id])
+    if @to_do.update(to_do_params)
+      redirect_to to_dos_url
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @to_do = ToDo.find_by(id: params[:id])
+    @to_do.destroy
+    redirect_to to_dos_url
+  end
+
+
+  private
+  def to_do_params
+    params.require(:to_do).permit(:title)
+  end
+
+end

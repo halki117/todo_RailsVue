547bb8e61eeef0319ed70349314e48f35b7ab7d7
halki <s.halki0203@icloud.com>
2020-06-14 20:38:56 +0900

    Update to_dos_controller.rb

--- app/controllers/api/v1/to_dos_controller.rb
+++ app/controllers/api/v1/to_dos_controller.rb
@@ -3,4 +3,10 @@ class Api::V1::ToDosController < ApplicationController
     @to_dos = ToDo.all
     # render json: to_dos
   end
+
+  def destroy
+    @to_do = Todo.find_by(id: params[:id])
+    @to_do.destroy
+    redirect_to to_do_url
+  end
 end
